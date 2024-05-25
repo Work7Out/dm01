0f0a99ed6534445fa4ba5001d5243f4e8395d42a
HayarovEd <edurd@rambler.ru>
2024-05-25 07:06:58 +0500

    v1_16

--- app/src/main/java/com/edurda77/dm01/domain/model/TabSlider.kt
+++ app/src/main/java/com/edurda77/dm01/domain/model/TabSlider.kt
@@ -14,30 +14,30 @@ val tabs = listOf(
         image = R.drawable.welcome_image_1,
         slider = R.drawable.welcome_sl_1,
         title = R.string.welcome_1_title,
-        description = R.string.welcome_1_title
+        description = R.string.welcome_1_dis
     ),
     TabSlider(
         image = R.drawable.welcome_image_2,
         slider = R.drawable.welcome_sl_2,
         title = R.string.welcome_2_title,
-        description = R.string.welcome_2_title
+        description = R.string.welcome_2_dis
     ),
     TabSlider(
         image = R.drawable.welcome_image_3,
         slider = R.drawable.welcome_sl_3,
         title = R.string.welcome_3_title,
-        description = R.string.welcome_3_title
+        description = R.string.welcome_3_dis
     ),
     TabSlider(
         image = R.drawable.welcome_image_4,
         slider = R.drawable.welcome_sl_4,
         title = R.string.welcome_4_title,
-        description = R.string.welcome_4_title
+        description = R.string.welcome_4_dis
     ),
     TabSlider(
         image = R.drawable.welcome_image_5,
         slider = null,
         title = R.string.welcome_5_title,
-        description = R.string.welcome_5_title
+        description = R.string.welcome_5_dis
     ),
 )
